7e32abe44ba2628e26942f13e6e2b9facd1f4e17
ozan onder <ozi_motaro@yahoo.com>
2025-12-25 14:00:11 +0000

    initial test

---
+++ learningapp/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace learningapp.Pages;
+
+public class IndexModel : PageModel
+{
+     public List<Course> Courses=new List<Course>();
+    private readonly ILogger<IndexModel> _logger;
+    private IConfiguration _configuration;
+    public IndexModel(ILogger<IndexModel> logger,IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration=configuration;
+    }
+
+}
